55f5a7c7a39fde38843e4a8fb3192701bc1a43ae
zspapp <zspapp@skawa.hu>
2022-05-07 15:47:32 +0200

    start transition effect

--- js/index.js
+++ js/index.js
@@ -1,11 +1,21 @@
 "use strict";
 
 const button = document.getElementsByClassName('button');
+const rating = document.getElementsByClassName('card__ratings');
 const front = document.getElementsByClassName('card__front');
 const back = document.getElementsByClassName('card__back');
-console.log(button);
+const backText = document.getElementsByClassName('card__text--selected');
 
-button[0].addEventListener("click", () => {
-    front[0].style.display = 'none';
-    back[0].style.display = 'block';
-});
+let selectedNumber = 0;
+
+(function addEventListeners() {
+    for (let i = 0; i < rating.length; i++) {
+        rating[i].addEventListener("click", () => {
+            selectedNumber = rating[i].value;
+        })
+    }
+    button[0].addEventListener("click", () => {
+        backText[0].innerHTML = `You selected ${selectedNumber} out of 5`
+        back[0].style.opacity = "1";
+    });
+})();
