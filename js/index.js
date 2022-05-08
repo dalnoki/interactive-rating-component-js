f63e0b14b55cd7cdb1256836e60c973e49d0e53f
zspapp <zspapp@skawa.hu>
2022-05-08 08:42:01 +0200

    fixed animation

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 "use strict";
 
-const button = document.getElementsByClassName('button');
+const button = document.getElementsByClassName('card__button');
 const rating = document.getElementsByClassName('card__ratings');
 const front = document.getElementsByClassName('card__front');
-const back = document.getElementsByClassName('card__back');
 const backText = document.getElementsByClassName('card__text--selected');
 
 let selectedNumber = 0;
@@ -16,6 +15,6 @@ let selectedNumber = 0;
     }
     button[0].addEventListener("click", () => {
         backText[0].innerHTML = `You selected ${selectedNumber} out of 5`
-        back[0].style.opacity = "1";
+        front[0].style.opacity = "0";
     });
 })();
